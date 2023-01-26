b137b9290c23a7fb7c4a18e13437feb22d48b707
Richard Chalk <barmybrit@gmail.com>
2023-01-26 15:38:43 +0100

    final 2023-01-26 2

---
+++ MyFirstRazorPagesApp/Pages/SupplierInfo.cshtml.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MyFirstRazorPagesApp.Models;
+
+namespace MyFirstRazorPagesApp.Pages
+{
+    public class SupplierInfoModel : PageModel
+    {
+        public string CompanyName { get; set; }
+        public string ContactName { get; set; }
+        public string City { get; set; }
+
+        private readonly NorthwindContext _dbContext;
+
+        public SupplierInfoModel(NorthwindContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public void OnGet(int suppId)
+        {
+            var supplierInfo = _dbContext.Suppliers.First(s=>s.SupplierId == suppId);
+            CompanyName = supplierInfo.CompanyName;
+            ContactName = supplierInfo.ContactName;
+            City = supplierInfo.City;
+        }
+    }
+}
